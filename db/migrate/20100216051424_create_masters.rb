4d6af863230e29c05a221d369a9454b51bb31153
knigh <knigh@stanford.edu>
2010-02-15 21:43:47 -0800

    Added migration files

---
+++ db/migrate/20100216051424_create_masters.rb
@@ -0,0 +1,19 @@
+class CreateMasters < ActiveRecord::Migration
+  def self.up
+    create_table :masters do |t|
+	t.column :logo_file, :string
+	t.column :pathways_logo, :string
+	t.column :informal_name, :string
+	t.column :formal_name, :string
+	t.column :self_description, :string
+	t.column :url, :string
+	t.column :alum_default_qs, :string
+	t.column :student_defailt_qs, :string
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :masters
+  end
+end
