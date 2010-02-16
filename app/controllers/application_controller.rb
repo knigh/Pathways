1f4e8e73a19304b1e2dbf2184008a9445fe4ef1f
knigh <knigh@stanford.edu>
2010-02-15 21:24:25 -0800

    Added master model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  
+  #$master = Masters.find(1)
+  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
