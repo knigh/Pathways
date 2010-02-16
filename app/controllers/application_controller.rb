6c0f2427b92c93d5fef104481f20feab040224c0
knigh <knigh@stanford.edu>
2010-02-15 23:21:11 -0800

    Using master db for pathways logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
-  #$master = Masters.find(1)
+  $master = Master.find(1)
   
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
