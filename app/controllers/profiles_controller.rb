889cb394c0b854bfc8a1546f39daf093b18f283b
knigh <knigh@stanford.edu>
2010-02-17 20:27:25 -0800

    Undo

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -172,17 +172,7 @@ class ProfilesController < ActionController::Base
 		# The search parameters are set in the commit variable
 		if (params[:commit] && (params[:commit] != ""))			
 			@searchResults = User.find(:all, :conditions => ['match(name,summary,alum_interview_text,student_interview_text,six_words) against (? with query expansion) and author != ?', params[:commit], 0], :order => 'name')
-			@jobs = Job.find(:all, :conditions => ['match(title,company,responsibilities) against (? with query expansion)', params[:commit]])
-			@jobs.each do |job|
-				user = User.find(job.user_id)
-				@searchResults.insert(user)
-			end
-			@degrees = Degree.find(:all, :conditions => ['match(major,degree) against (? with query expansion)', params[:commit]])
-			@degrees.each do |degree|
-				user = User.find(degree.user_id)
-				@searchResults.insert(user)
-			end
-			@searchResults = @searchResults.uniq.sort
+
 		else
 			@searchResults = User.find(:all, :conditions => ['author != ?', 0], :order => 'views DESC', :limit => 10)
 		end	
