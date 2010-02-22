1e9fc5e1097172e05b2b43bd242be4d8cbf3c3e5
knigh <knigh@stanford.edu>
2010-02-22 00:20:39 -0800

    fixed typo

--- db/migrate/20100222080842_users.rb
+++ db/migrate/20100222080842_users.rb
@@ -1,7 +1,7 @@
 class Users < ActiveRecord::Migration
   def self.up
-	add_column :approved, :integer, :default => 0
-	add_column :like_list, :string, :default => ""
+	add_column :users, :approved, :integer, :default => 0
+	add_column :users, like_list, :string, :default => ""
   end
 
   def self.down
