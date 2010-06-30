0217ad2398cf5e9ddec68ec68c1892f9d6504fa6
Coram Bryant <cabryant@stanford.edu>
2010-06-30 02:27:27 -0700

    reverting incomplete advanced question support.  Comments describing how to add advanced functionality remain in place.  The only files that need to be updated to add this behavior is the profile controller and the edit page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -38,7 +38,10 @@ class ProfilesController < ActionController::Base
 				@interview_text = @user.student_interview_text
 			end
 			
-			@questions = Question.find(:all, :conditions => ["user_id = ?", id])
+			# For ADVANCED_QUESTION_FUNCTIONALITY, uncomment this line.  On the edit page, format this set of questions
+			# such that the user can answer each individually.  Once a question is answered, the question asker should
+			# be notified via email (or in their account?), and the question + response should be added to the interview text.
+			# @questions = Question.find(:all, :conditions => ["user_id = ?", id])
 			
 			@max_title_word_count = 6
 			@max_summary_length = 250
@@ -243,20 +246,21 @@ class ProfilesController < ActionController::Base
 				@interview_text = @user.student_interview_text
 			end
 			
-			# add all questions to the viewable text
-			new_questions = Question.find(:all, :conditions => ["user_id =?", id])
-			if ((new_questions != nil) && (new_questions.length > 0))
-				
-				if (@interview_text[(@interview_text.length-1)..(@interview_text.length-1)] != "\n")
-					@interview_text = @interview_text + "\n"
-				end
-				
-				new_questions.each do |question|
-					# get the user who asked the question
-					asker = User.find(question.asker_id)
-					@interview_text = @interview_text + "\n" + '[[' + question.question + ' (posted by <' + asker.name + '|/profiles/view/' + question.asker_id.to_s + '>)' + ']]' + "\n"
-				end
-			end
+			# For ADVANCED_QUESTION_FUNCTIONALITY, uncomment this block of code.
+			## add all questions to the viewable text
+			# new_questions = Question.find(:all, :conditions => ["user_id =?", id])
+			#if ((new_questions != nil) && (new_questions.length > 0))
+			#	
+			#	if (@interview_text[(@interview_text.length-1)..(@interview_text.length-1)] != "\n")
+			#		@interview_text = @interview_text + "\n"
+			#	end
+			#	
+			#	new_questions.each do |question|
+			#		# get the user who asked the question
+			#		asker = User.find(question.asker_id)
+			#		@interview_text = @interview_text + "\n" + '[[' + question.question + ' (posted by <' + asker.name + '|/profiles/view/' + question.asker_id.to_s + '>)' + ']]' + "\n"
+			#	end
+			#end
 			
 			@interview_text.gsub!(/<.*\|.*>/) {|match| '<a href="' + match[(match.index('|') + 1)..(match.length - 2)] + '" target="_blank">' + match[1..match.index('|') - 1] + '</a>'}
 			@interview_text.gsub!(/\[\[.*\]\]/) {|match| '<span class="question">' + match[2..(match.length - 3)] + '</span>'}
@@ -292,28 +296,72 @@ class ProfilesController < ActionController::Base
 		redirect_to("/profiles/view/#{@user[:id]}")	
 	end
 	
+	# For ADVANCED_QUESTION_FUNCTIONALITY, uncomment this entire
+	# method, and erase the one below
+	#def post_question
+	#	createNewLogEntry(request.request_uri)
+	#
+	#	@user = User.find(params[:id])
+	#	new_question = params[:new_question]
+	#	
+	#	if (session[:user_id] != @user.id && session[:user_id] != @user.author)
+	#		@user.views = @user.views - 1
+	#		author = User.find(@user.author)
+	#		author.total_views = author.total_views - 1
+	#		author.save
+	#	end
+	#	
+	#	@question = Question.new
+	#	@question.user_id = @user.id
+	#	@question.asker_id = session["#{$master.url}_id"]
+	#	@question.question = new_question
+	#	@question.date_added = Time.now
+	#	
+	#	@question.save
+	#	@user.save
+	#	
+	#	redirect_to("/profiles/view/#{@user[:id]}")
+	#end
+	
+	# For ADVANCED_QUESTION_FUNCTIONALITY, erase this entire
+	# method, and uncomment the one above
 	def post_question
 		createNewLogEntry(request.request_uri)
 
 		@user = User.find(params[:id])
 		new_question = params[:new_question]
-		
+
+		asker = ""
+
+		if session["#{$master.url}_id"]
+			user = User.find(session["#{$master.url}_id"])
+			asker = ' (posted by <' + user.name + '|/profiles/view/' + user.id.to_s + '>)'
+		end
+
+		#Adds an extra line break if needed
+		text = @user.alum_interview_text
+		if (text[(text.length-1)..(text.length-1)] != "\n")
+			@user.alum_interview_text = text + "\n"
+		end
+
+		text = @user.student_interview_text
+		if (text[(text.length-1)..(text.length-1)] != "\n")
+			@user.student_interview_text = text + "\n"
+		end
+
+		@user.alum_interview_text = @user.alum_interview_text + "\n[[" + new_question + asker + "]]\n"
+		@user.student_interview_text = @user.student_interview_text + "\n[[" + new_question + asker + "]]\n"
+
 		if (session[:user_id] != @user.id && session[:user_id] != @user.author)
 			@user.views = @user.views - 1
 			author = User.find(@user.author)
 			author.total_views = author.total_views - 1
 			author.save
 		end
-		
-		@question = Question.new
-		@question.user_id = @user.id
-		@question.asker_id = session["#{$master.url}_id"]
-		@question.question = new_question
-		@question.date_added = Time.now
-		
-		@question.save
+
+		@user.question_asked = Time.now
 		@user.save
-		
+	
 		redirect_to("/profiles/view/#{@user[:id]}")
 	end
 	
